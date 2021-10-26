735076f4906790de84be49b2bacdfabf4604808d
Raquel França <89147475+Raqfranca@users.noreply.github.com>
2021-10-25 22:47:31 -0300

    Add files via upload
    Mesa de trabalho - Aula 4

---
+++ MesaDeTrabalho.js
@@ -0,0 +1,18 @@
+var nome = "Aline";
+
+var sobrenome = "Paula";
+
+var idade = 27;
+
+var peso = 63.7;
+
+var altura = 1.53; 
+
+var plano = "Não Possui";
+
+var imc = peso/(altura*altura);
+
+console.log (nome, sobrenome, "tem", idade, "anos e seu índice de massa corporal é de", imc);
+
+
+
